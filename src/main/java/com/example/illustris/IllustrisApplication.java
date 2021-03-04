2d78caca8c19467298545b9f2565a5bd55caa59c
ajfurey <ajfurey@users.noreply.github.com>
2021-03-04 17:35:26 -0500

    setup

---
+++ src/main/java/com/example/illustris/IllustrisApplication.java
@@ -0,0 +1,13 @@
+package com.example.illustris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllustrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllustrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/illustris/IllustrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.illustris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllustrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
