f239f567c77dcfdb6b5a075fe3fa1c404ef031f6
ajfurey <ajfurey@users.noreply.github.com>
2021-03-24 21:29:47 -0400

    forgot usename and password setter

--- src/main/java/com/example/illustris/user/User.java
+++ src/main/java/com/example/illustris/user/User.java
@@ -144,12 +144,19 @@ public class User implements UserDetails{
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
     @Override
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     @Override
     public boolean isAccountNonExpired() {
