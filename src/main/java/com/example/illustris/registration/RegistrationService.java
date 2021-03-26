4279bc2b7b64b48b4e4cd39b2477a837dee910d7
ajfurey <ajfurey@users.noreply.github.com>
2021-03-25 21:25:26 -0400

    Registration

---
+++ src/main/java/com/example/illustris/registration/RegistrationService.java
@@ -0,0 +1,91 @@
+package com.example.illustris.registration;
+
+import org.springframework.stereotype.Service;
+import com.example.illustris.user.User;
+import com.example.illustris.user.UserService;
+import com.example.illustris.user.UserRole;
+import com.example.illustris.email.EmailSender;
+import com.example.illustris.registration.token.ConfirmationToken;
+import com.example.illustris.registration.token.ConfirmationTokenService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class RegistrationService {
+    
+    private final UserService userService;
+    private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final EmailSender emailSender;
+
+    public RegistrationService(UserService userService, EmailValidator emailValidator,
+            ConfirmationTokenService confirmationTokenService, EmailSender emailSender) {
+        this.userService = userService;
+        this.emailValidator = emailValidator;
+        this.confirmationTokenService = confirmationTokenService;
+        this.emailSender = emailSender;
+    }
+
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+
+        String token = userService.signUpUser(
+                new User(request.getFirstName(),request.getLastName(),request.getEmail(),
+                        request.getPassword(),request.getUsername(),request.getPhone(),
+                        request.getDob(),UserRole.USER,request.getPosition(),request.getHireDate()
+                )
+        );
+
+        String link = "http://localhost:8080/api/v1/Registration/confirm?token=" + token;
+        emailSender.send(
+                request.getEmail(),
+                buildEmail(request.getFirstName(), link));
+
+        return token;
+    }
+
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token)
+                .orElseThrow(() ->
+                        new IllegalStateException("token not found"));
+
+        if (confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        userService.enableUser(
+                confirmationToken.getUser().getEmail());
+        return "confirmed";
+    }
+
+    private String buildEmail(String name, String link) {
+        return 
+        "<!DOCTYPE HTML>\n"+
+        "<head>\n"+
+            "<title>Getting Started: Serving Web Content</title>\n"+
+            "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n"+
+        "</head>\n"+
+        "<body>\n"+
+            "<p>Hi there</p>\n"+
+           "<p>Here's the link  "+link+"</p>\n"+
+        "</body>\n"+
+        "</html>";
+    }
+
+
+}
---
+++ src/main/java/com/example/illustris/registration/token/ConfirmationToken.java
@@ -0,0 +1,99 @@
+package com.example.illustris.registration.token;
+
+import com.example.illustris.user.User;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class ConfirmationToken {
+    @SequenceGenerator(
+        name = "confirmation_token_sequence", 
+        sequenceName = "confirmation_token_sequence",allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "confirmation_token_sequence")
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false,name = "user_id")
+    private User user;
+    
+    public ConfirmationToken() {
+    }
+    
+    public ConfirmationToken(Long id, String token, LocalDateTime createdAt, LocalDateTime expiresAt,
+            LocalDateTime confirmedAt, User user) {
+        this.id = id;
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.confirmedAt = confirmedAt;
+        this.user = user;
+    }
+
+    public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt, User user) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public LocalDateTime getConfirmedAt() {
+        return confirmedAt;
+    }
+
+    public void setConfirmedAt(LocalDateTime confirmedAt) {
+        this.confirmedAt = confirmedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }    
+}
